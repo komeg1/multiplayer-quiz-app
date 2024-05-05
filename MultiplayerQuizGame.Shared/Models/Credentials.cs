8e9745c4c4cfdeab5e6309af54d5b768516f5ff3
Tomek Krezymon <tomekkrezymon@gmail.com>
2024-05-06 01:26:43 +0200

    add simple register/login validation, add question choices randomization on each question

--- MultiplayerQuizGame.Shared/Models/Credentials.cs
+++ MultiplayerQuizGame.Shared/Models/Credentials.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace MultiplayerQuizGame.Shared.Models
 {
     public class Credentials
     {
+        [Required]
+        [MinLength(5)]
         public string Username { get; set; }
+        [Required]
+        [MinLength(8)]
         public string Password { get; set; }
     }
 }
